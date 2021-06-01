1b235b5b6b73e4a595ef2834a47c077e19ebcd37
mani333007 <mani333007@gmail.com>
2021-06-01 16:54:34 +0530

    added pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Switch, Route } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import Status from './pages/Status'
+import Login from './pages/Login'
+import LastLogins from './pages/LastLogins'
+
 function App() {
     return (
-        <div>
-            React and Tailwind Css App
-        </div>
+        <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/status" component={Status} />
+            <Route path="/login" component={Login} />
+            <Route path="/lastlogins" component={LastLogins} />
+        </Switch>
     )
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            Home Chat
+        </div>
+    )
+}
+
+export default Home
