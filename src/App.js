535874b1f6d316cb050ca4ff666fc7bde69d920c
mani333007 <mani333007@gmail.com>
2021-07-05 16:18:14 +0530

    context added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ function App() {
     return (
         <div>
            <h1 className="text-3xl m-5">
-               Netlify apps
-            </h1>
+               Netlify
+           </h1>
+           <p>Upload your web app to netlify hosting and show in entire internet world</p>
         </div>
     )
 }
